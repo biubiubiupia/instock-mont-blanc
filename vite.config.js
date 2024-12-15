66d9e4bec2423931e0b0bd028b22ade2e5347fac
Tina Feng <contact@tinafeng.com>
2024-12-14 19:40:22 -0500

    update vite.config.js base

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/instock-mont-blanc/', // The repository name (relative path on GitHub Pages)
   plugins: [react()],
 })
